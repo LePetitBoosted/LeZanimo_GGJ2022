514ee6b3c8be50b0f82d7fd2e18f01cff3ba1864
LePetitBoosted <ringevaleliot@hotmail.fr>
2022-01-29 15:46:55 +0100

    Parrry behaviour & TMP imported

--- GGJ2022_UnityProject/Assets/Scripts/PlayerBall.cs
+++ GGJ2022_UnityProject/Assets/Scripts/PlayerBall.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerBall : MonoBehaviour
 {
+    [SerializeField] GameObject dashState;
     [SerializeField] GameObject ball;
     public void LooseBall() 
     {
-        ball.transform.position = transform.position;
-        ball.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        Vector2 loseDirection = new Vector2(Random.Range(-0.25f, 0.25f), 1);
-        ball.GetComponent<Rigidbody2D>().AddForce(loseDirection * 10f);
-        ball.SetActive(true);
+        if (dashState.activeSelf == false)
+        {
+            ball.transform.position = transform.position;
+            ball.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            Vector2 loseDirection = new Vector2(Random.Range(-0.25f, 0.25f), 1);
+            ball.GetComponent<Rigidbody2D>().AddForce(loseDirection * 10f);
+            ball.SetActive(true);
 
-        gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
     }
 }

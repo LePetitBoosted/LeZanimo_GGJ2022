bf31944b2f9d581d0dfaa62d3784a9b7bae9f05a
LePetitBoosted <ringevaleliot@hotmail.fr>
2022-01-29 23:42:41 +0100

    Difficulty System for Malus

--- GGJ2022_UnityProject/Assets/Scripts/MalusManager.cs
+++ GGJ2022_UnityProject/Assets/Scripts/MalusManager.cs
@@ -1,27 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MalusManager : MonoBehaviour
 {
-    [SerializeField] GameObject PlayerOne;
-    [SerializeField] GameObject PlayerTwo;
+    bool allStar;
+
+    GameManager gameManager;
 
     [SerializeField] List<GameObject> malusList = new List<GameObject>();
     int currentMalusIndex;
 
     public GameObject targetPlayer;
 
+    private void Awake()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
     public void GiveMalus(GameObject player) 
     {
-        ChooseMalus();
-        malusList[currentMalusIndex].SetActive(true);
         targetPlayer = player;
+        ChooseMalus();
     }
 
     void ChooseMalus() 
     {
-        currentMalusIndex = Random.Range(0, malusList.Count);
+        if (allStar == false)
+        {
+            float targetLevel;
+            if (targetPlayer.GetComponent<PlayerControls>().playerNumber == PlayerNumber.PlayerOne)
+            {
+                targetLevel = gameManager.playerOneScore;
+            }
+            else 
+            {
+                targetLevel = gameManager.playerTwoScore;
+            }
+
+            List<GameObject> possibleMalus = new List<GameObject>();
+            foreach(GameObject malus in malusList) 
+            {
+                if (Enumerable.Range(Mathf.RoundToInt(malus.GetComponent<MalusBase>().malusLevel.x), Mathf.RoundToInt(malus.GetComponent<MalusBase>().malusLevel.y)).Contains(Mathf.RoundToInt(targetLevel))) 
+                {
+                    possibleMalus.Add(malus);
+                }
+            }
+
+            currentMalusIndex = Random.Range(0, possibleMalus.Count);
+            possibleMalus[currentMalusIndex].SetActive(true);
+        }
+        else 
+        {
+            currentMalusIndex = Random.Range(0, malusList.Count);
+            malusList[currentMalusIndex].SetActive(true);
+        }
     }
 
     public void EndMalus() 
@@ -29,4 +63,3 @@ public class MalusManager : MonoBehaviour
         malusList[currentMalusIndex].SetActive(false);
     }
 }
-
